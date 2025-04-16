f9883fdc2a66ebc212295ddaf5e3d070d3ecd2c9
Sergiy <sergiykochenko@gmail.com>
2025-04-16 01:55:28 +0100

    Update npm run build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,16 +16,6 @@ const siteId = 6373343;
 const hotjarVersion = 6;
 Hotjar.init(siteId, hotjarVersion);
 
-// Hotjar Tracking Code for Web-Shop
-(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:6373343,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
