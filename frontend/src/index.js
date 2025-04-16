894f9d2214bcd00223bf7b2a2b0e18e536ced4bd
Sergiy <sergiykochenko@gmail.com>
2025-04-16 01:48:31 +0100

    npm run build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,16 +9,22 @@ import './index.css'
 import './bootstrap.min.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Hotjar from '@hotjar/browser';
+
+// Initialize Hotjar
+const siteId = 6373343;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 // Hotjar Tracking Code for Web-Shop
 (function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:process.env.REACT_APP_HOTJAR_ID,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6373343,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
